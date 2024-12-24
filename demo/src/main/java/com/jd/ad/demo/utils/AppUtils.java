ad8b2e600054861ccb14aaa887245328950e1498
haorongrong3 <haorongrong3@jd.com>
2024-12-24 18:08:14 +0800

    Automatic: this is update for release

---
+++ demo/src/main/java/com/jd/ad/demo/utils/AppUtils.java
@@ -0,0 +1,36 @@
+package com.jd.ad.demo.utils;
+
+import android.app.ActivityManager;
+import android.content.Context;
+
+import java.util.List;
+
+public class AppUtils {
+    /**
+     * 获取当前进程名
+     *
+     * @param context 上下文
+     * @return
+     */
+    public static String getCurProcessName(Context context) {
+        // 获取此进程的标识符
+        try {
+            int pid = android.os.Process.myPid();
+            // 获取活动管理器
+            ActivityManager activityManager = (ActivityManager)
+                    context.getSystemService(Context.ACTIVITY_SERVICE);
+            // 从应用程序进程列表找到当前进程，是：返回当前进程名
+            List<ActivityManager.RunningAppProcessInfo> runningAppProcesses = activityManager.getRunningAppProcesses();
+            if (runningAppProcesses != null) {
+                for (ActivityManager.RunningAppProcessInfo appProcess : runningAppProcesses) {
+                    if (appProcess.pid == pid) {
+                        return appProcess.processName;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+}
